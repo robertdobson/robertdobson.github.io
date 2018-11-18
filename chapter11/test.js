16d6f10c6ae3a991661f8a167188059f1f1d1b82
robertdobson <33889822+robertdobson@users.noreply.github.com>
2018-11-18 13:24:44 -0700

    Added Chapter 11 assignment

---
+++ chapter11/test.js
@@ -0,0 +1,15 @@
+/* 
+  CIS166AA: Chapter 11 Case Project Website
+  Author: Robert Dobson
+  Date: 11/11/2018
+*/
+// JavaScript Document
+
+ForexTicker.init(
+    {
+        pairs: ["USDGBP", "USDEUR", "USDCHF", "USDCAD", "USDAUD", "USDNZD", "USDTRY", "BTCUSD"],
+        theme: "dark", //this can be "light" or "dark"
+        margin: "auto",
+        width: "40%",
+        height: "600px",
+  });
